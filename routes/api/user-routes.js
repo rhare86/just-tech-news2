3bb299c0fb5e9a7daffb551dc563908065cad863
rhare86 <rhare86@gmail.com>
2022-08-02 19:12:42 -0500

    hashed pwrd

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,13 +55,15 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
-        where: {
-            id: req.params.id
-        }
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
     })
+  
         .then(dbUserData => {
             if (!dbUserData[0]) {
                 res.status(404).json({ message: 'No user found with this id' });
